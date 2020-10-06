a866231319df0d4feb8e673248d83aa3c057b00c
Rahul Kadyan <hi@znck.me>
2020-10-06 07:40:50 +0530

    chore: setup lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,6 +2,5 @@ module.exports = {
   "*.{ts}": [
     "jest --bail --findRelatedTests",
     "eslint --cache --fix",
-    "prettier --write",
-  ],"*.{js}": ["eslint --cache --fix", "prettier --write"], "*.{json,prettierrc}": ["prettier --parser json --write"], "*.{yaml,yml}": ["prettier --parser yaml --write"],
+    "prettier --write",],"*.{js}": ["eslint --cache --fix", "prettier --write"], "*.{json,prettierrc}": ["prettier --parser json --write"], "*.{yaml,yml}": ["prettier --parser yaml --write"],
 }
