01c9d870ffc444d9aa3f3fef15da9e591ca62de8
Rahul Kadyan <hi@znck.me>
2020-10-07 11:29:09 +0530

    feat: add tsconfig paths resolution for rollup and jest

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import typescript from "@rollup/plugin-typescript"
 import dts from "rollup-plugin-dts"
+import { typescriptPaths } from "rollup-plugin-typescript-paths"
 import { dependencies } from "./package.json"
 
 const external = ["rxjs/operators", ...Array.from(Object.keys(dependencies))]
@@ -13,13 +14,26 @@ export default [
   },
   {
     input: "src/index.ts",
-    output: { format: "esm", file: "dist/module-template.esm.js", sourcemap: true },
+    output: {
+      format: "esm",
+      file: "dist/module-template.esm.js",
+      sourcemap: true,
+    },
     plugins: [typescript()],
     external,
   },
   {
     input: "src/index.ts",
-    output: { format: "esm", file: "dist/module-template.d.ts", sourcemap: true },
-    plugins: [dts()],
+    output: {
+      format: "esm",
+      file: "dist/module-template.d.ts",
+      sourcemap: true,
+    },
+    plugins: [
+      typescriptPaths({
+        transform: (path) => path.replace(/\.js$/, ".ts"),
+      }),
+      dts(),
+    ],
   },
 ]
